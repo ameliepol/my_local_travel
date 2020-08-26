8ec2e7bc40831f3d5cbc6e016d337132fc18a3d3
Jeremy Glize <le-schlouze@hotmail.fr>
2020-08-26 17:52:37 +0200

    homepage

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,17 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema.define(version: 2020_08_25_152737) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -104,10 +116,8 @@ ActiveRecord::Schema.define(version: 2020_08_25_152737) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
-
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "activities", "availabilities"
-
+  add_foreign_key "availabilities", "activities"
   add_foreign_key "days", "trips"
   add_foreign_key "reviews", "activities"
   add_foreign_key "reviews", "users"
